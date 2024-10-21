6fec9f64768efb49a0bbbc886c15f19ccc26d956
josh <woodabmxhd@gmail.com>
2024-10-21 22:45:26 +1100

    added remove all favourites buttton and loading spinner

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,22 +3,27 @@ let isFavouritesActive = false;
 document.addEventListener("DOMContentLoaded", async () => {
   const searchInput = document.getElementById("song-search");
   const results = document.getElementById("results");
-  const loading = document.getElementById("loading");
+  const loading = document.getElementById("loading-container");
   const recent = document.getElementById("recent");
   const favourites = document.getElementById("favourites");
   const fragment2 = document.createDocumentFragment();
   const favNavBtn = document.getElementById("fav-nav-btn");
   const recentNavBtn = document.getElementById("recent-nav-btn");
   const searchNavBtn = document.getElementById("search-nav-btn");
+  const removeFavsBtn = document.getElementById("remove-favourites");
+  const removeFavsContainer = document.getElementById(
+    "remove-favourites-container"
+  );
   let songs = [];
   console.log(JSON.parse(localStorage.getItem("chartID")));
   try {
-    loading.style.display = "block";
+    loading.style.display = "flex";
     searchInput.disabled = true;
     const response = await fetch("/search");
     if (!response.ok) {
       throw new Error(`Fetch error: ${response.status}`);
     }
+
     songs = await response.json();
     loading.style.display = "none";
     searchInput.disabled = false;
@@ -52,9 +57,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       if (searchInput.value == "") {
         showRecent(songs, fragment2, recent);
+        removeFavsContainer.style.display = "none";
       } else {
         const filteredSongs = filterSongs(songs, `${searchInput.value}`);
         renderSongs(filteredSongs, fragment, results);
+        removeFavsContainer.style.display = "none";
       }
     }, 300)
   );
@@ -69,6 +76,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     setActiveButton(recentNavBtn);
     isFavouritesActive = false;
     showRecent(songs, fragment2, recent);
+    removeFavsContainer.style.display = "none";
   });
 
   searchNavBtn.addEventListener("click", () => {
@@ -77,11 +85,18 @@ document.addEventListener("DOMContentLoaded", async () => {
     isFavouritesActive = false;
     const filteredSongs = filterSongs(songs, `${searchInput.value}`);
     renderSongs(filteredSongs, fragment, results);
+    removeFavsContainer.style.display = "none";
   });
 
   document.addEventListener("renderFavourites", () => {
     favourites.innerHTML = "";
     showFavourites(songs, fragment2, favourites);
+  });
+
+  removeFavsBtn.addEventListener("click", () => {
+    localStorage.setItem("chartID", JSON.stringify([]));
+    const event = new Event("renderFavourites");
+    document.dispatchEvent(event);
   });
 });
 
@@ -100,7 +115,7 @@ function filterRecentSongs(array) {
 }
 
 function renderSongs(array, fragment, container) {
-  const favourites = JSON.parse(localStorage.getItem("chartID") || []);
+  const favourites = JSON.parse(localStorage.getItem("chartID") || "[]");
   const length = array.length;
   console.log("results:", length);
   console.log(container.id);
@@ -160,17 +175,24 @@ function showRecent(array, fragment, container) {
 }
 
 function showFavourites(array, fragment, container) {
-  let favourites = JSON.parse(localStorage.getItem("chartID")) || [];
+  let favourites = JSON.parse(localStorage.getItem("chartID") || "[]");
   let favouriteSongs = array.filter((element) =>
     favourites.includes(element.id)
   );
   console.log("favourite songs:", favouriteSongs);
+  console.log("Number of favourite songs:", favouriteSongs.length);
   renderSongs(favouriteSongs, fragment, container);
+  console.log(
+    "Setting remove favs container display to:",
+    favouriteSongs.length > 0 ? "block" : "none"
+  );
+  document.getElementById("remove-favourites-container").style.display =
+    favouriteSongs.length > 0 ? "block" : "none";
 }
 
 function addFavourite(chartid) {
   let favourites = [];
-  let currentFavourites = JSON.parse(localStorage.getItem("chartID")) || [];
+  let currentFavourites = JSON.parse(localStorage.getItem("chartID") || "[]");
   const itemIndex = currentFavourites.indexOf(chartid);
   const messageElement =
     document.getElementById("favouriteMessage") || createMessageElement();
